67d2c46d998688626decf4c5d30a86c71ba6203a
y-akamatsu <ak1985tk@gmail.com>
2018-12-18 21:06:30 +0900

    DataTimeメソッド

--- test.php
+++ test.php
@@ -1,7 +1,16 @@
-<? php 
+<?php 
 
 //変数の定義  
-$weekday = array('日','月','火', '水', '木', '金','土');
+$weekday = array('日','月','火','水','木','金','土');
 $day = 1;
+
+//new DateTimeでと今日の日付けを取得
+$datatime = new DateTime("Asia/Tokyo");
+
+//現在の年、月を取得
+$year = $datatime->format('Y');
+$month = $datatime->format('m');
+
+//現在の月の1日目の曜日を割り出すために現在の日付を１日目として設定
 
 ?>
